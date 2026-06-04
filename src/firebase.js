d80c27e42a881684490043254492fc6d5bafd99d
mistywynn10 <mistywynn10@gmail.com>
2026-06-04 13:51:43 -0400

    Responsiveness

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,7 @@ import {
     getAuth,
     signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCk6uB-mXYVhpnatRH2l4zPU9VRWzyO6rU",
@@ -31,7 +32,7 @@ const signUp = async (name, email, password) => {
         });
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
     }
 }
 
@@ -40,7 +41,7 @@ const login = async (email, password) => {
        await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
     }
 }
 
